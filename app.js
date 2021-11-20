30cfba7205c873ef04e0a4bbde70cca9a69117c3
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2021-11-20 16:27:14 +0530

    feat : vowels & consonant counter
    App to count vowels & consonant in a text

---
+++ app.js
@@ -0,0 +1,69 @@
+// const inputText = document.querySelector(".input-text")
+// const btnCheck = document.querySelector(".btn-check")
+// const outputDiv = document.querySelector(".output")
+
+// var vowelList = ['A', 'E', 'I', 'O', 'U']
+
+// btnCheck.addEventListener('click', clickHandler)
+
+
+// function clickHandler(){
+//     var vowelCounter = 0;
+//     var consonantCounter = 0;
+//     var text = inputText.value.toUpperCase();
+    
+//     for(i=0; i<text.length;i++){
+      
+//         if(vowelList.includes(text.charAt(i))){
+//           vowelCounter++;
+//           outputDiv.innerText = "Vowel Count : " +vowelCounter
+//         }
+//         else{
+//           consonantCounter++;
+//           outputDiv.innerText = "Consonant Count : " +consonantCounter 
+//         }
+
+//         outputDiv.innerText = "Vowel Count : " +vowelCounter + " Consonant Count : " +consonantCounter
+        
+//         }
+//     }
+// Create a web app, which you have to create two btns one is start, second is stop, and input text on clicking of start btn the text color should change in every 1 sec and on clicking stop it should stop at a particular color.
+
+// const inputText = document.querySelector(".input-text");
+// const btnStart = document.querySelector(".start");
+// const btnStop = document.querySelector(".stop");
+// const outputDiv = document.querySelector(".output");
+
+// var colorList = ['red', 'blue', 'green', 'yellow', 'violet', 'black', 'maroon', 'pink'];
+
+// btnStart.addEventListener("click",  startHandler)
+// btnStop.addEventListener('click', stopHandler)
+
+// function startHandler(){
+//     timer = setInterval(startHandler, 1000)
+//     var text = inputText.value
+//     var number = Math.random();
+//     number = Math.floor(number*colorList.length)
+// //    for(i=0;i<colorList.length;i++){
+// //        outputDiv.style.color = colorList[number]
+// //        outputDiv.innerText = text;
+
+
+//     timer = setInterval(function(){
+//       outputDiv.style.color = colorList[number];
+//     },)
+//     console.log(text)
+//       outputDiv.innerHTML = text;  
+    
+   
+   
+// }
+
+// function stopHandler(){
+//     clearInterval(timer)
+// }
+
+
+
+
+
