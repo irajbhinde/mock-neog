a99b64cf453a530d4ed4ac1fbf729c5eb8521fa3
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-01-28 22:43:59 +0530

    practice
    practice

--- app.js
+++ app.js
@@ -29,39 +29,58 @@
 //     }
 // Create a web app, which you have to create two btns one is start, second is stop, and input text on clicking of start btn the text color should change in every 1 sec and on clicking stop it should stop at a particular color.
 
-// const inputText = document.querySelector(".input-text");
-// const btnStart = document.querySelector(".start");
-// const btnStop = document.querySelector(".stop");
-// const outputDiv = document.querySelector(".output");
+const inputText = document.querySelector(".input-text");
+const btnStart = document.querySelector(".start");
+const btnStop = document.querySelector(".stop");
+const outputDiv = document.querySelector(".output");
+var handler;
 
-// var colorList = ['red', 'blue', 'green', 'yellow', 'violet', 'black', 'maroon', 'pink'];
-
-// btnStart.addEventListener("click",  startHandler)
-// btnStop.addEventListener('click', stopHandler)
-
-// function startHandler(){
-//     timer = setInterval(startHandler, 1000)
-//     var text = inputText.value
-//     var number = Math.random();
-//     number = Math.floor(number*colorList.length)
-// //    for(i=0;i<colorList.length;i++){
-// //        outputDiv.style.color = colorList[number]
-// //        outputDiv.innerText = text;
+var colorList = ['red', 'blue', 'green', 'yellow', 'violet', 'black', 'maroon', 'pink'];
 
 
-//     timer = setInterval(function(){
-//       outputDiv.style.color = colorList[number];
-//     },)
-//     console.log(text)
-//       outputDiv.innerHTML = text;  
+
+function randomColor(){
+  var number = Math.random();
+  number = Math.floor(number*colorList.length)
+  return number;
+} 
+
+
+const startHandler = () => {
+      var text = inputText.value;
+      handler = setInterval(function (){
+      outputDiv.style.color = colorList[randomColor()]
+    }, 1000)
+    
+    outputDiv.innerText = text;
+    
+      
+   }
+
+   btnStart.addEventListener("click",  startHandler)
+   btnStop.addEventListener('click', () => {
+  clearInterval(handler)
+})
+
+   
+
+
+
+
+    // timer = setInterval(function(){
+    //   outputDiv.style.color = colorList[number];
+    // },)
+    // console.log(text)
+    //   outputDiv.innerHTML = text;  
     
    
    
-// }
 
-// function stopHandler(){
-//     clearInterval(timer)
-// }
+
+
+
+
+
 
 
 
